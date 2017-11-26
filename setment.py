51ff31ed90a9e0f19ba85a3dadc453ea8c2c206c
TSR2 <th0928367718@hotmail.com>
2017-11-26 20:13:22 +0800

    1

--- setment.py
+++ setment.py
@@ -4,7 +4,7 @@ import jieba
 import csv
 import toolz
 
-with open('C:/Users/TSR/Desktop/project data\ptt_tag_V1.csv') as csvfile:
+with open('C:/Users/TSR/Desktop/project data/ptt_tag_V1.csv') as csvfile:
     spamreader = csv.reader(csvfile)
     score = []
     content = []
@@ -16,6 +16,14 @@ with open('C:/Users/TSR/Desktop/project data\ptt_tag_V1.csv') as csvfile:
         elif i >= 2:
             score.append(row[2])
             content.append(row[3])
+
+
+with open('C:/Users/TSR/Desktop/python/stopword.txt',encoding = 'utf8') as stopfile:
+    spamreader = stopfile.read()
+
+ 
+stop1 = spamreader.split('\n')
+
 
 
 x = {}
@@ -33,10 +41,29 @@ aaa = toolz.valmap(lambda x:dict(Counter(list(jieba.cut(x)))), x)
 #contents_cut = list(contents_cut)
 
 
-        
+def get_str_score(x):
+    count = []
+    weight = []
+    score = []
+    for key in aaa.keys():
+        if x in aaa[key] :
+            count.append(aaa[key][x])
+            weight.append(int(key))
+        else :
+            count.append(0)
+            weight.append(0)
+    for j in range(len(weight)):
+        score.append(count[j]*weight[j])
+    
+    return sum(score)/sum(count)
+
 def get_content_score(x):
     str_cut = list(jieba.cut(x))
-    total_count = map(lambda x: contents_cut[1][x] ,str_cut)
-    return list(total_count)
+    total_count = list(map(get_str_score,str_cut))
+    return sum(total_count)/len(total_count)
 
-ss = get_content_score(content[1])
+score_final = []
+for i in range(10):
+    score_final.append([get_content_score(content[i]),score[i]])
+
+print(score_final)
